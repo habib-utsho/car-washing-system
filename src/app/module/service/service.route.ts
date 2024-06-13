607d810c54989559ff7a39687ee3c3e98d3ecd15
Source-Code-007 <utsho926@gmail.com>
2024-06-13 22:43:47 +0600

    service zod validate modified

--- src/app/module/service/service.route.ts
+++ src/app/module/service/service.route.ts
@@ -1,14 +1,22 @@
-import { Router } from "express";
-import { serviceControllers } from "./service.controller";
+import { Router } from 'express'
+import { serviceControllers } from './service.controller'
+import zodValidateHandler from '../../middleware/zodValidateHandler'
+import { serviceZodSchema } from './service.validate'
 
-const  router = Router()
+const router = Router()
 
-
-router.post('/', serviceControllers.createService)
+router.post(
+  '/',
+  zodValidateHandler(serviceZodSchema.createServiceZodSchema),
+  serviceControllers.createService,
+)
 router.get('/:id', serviceControllers.getAllService)
 router.get('/', serviceControllers.getServiceById)
 router.delete('/', serviceControllers.deleteServiceById)
-router.patch('/', serviceControllers.updateServiceById)
+router.patch(
+  '/',
+  zodValidateHandler(serviceZodSchema.updateServiceZodSchema),
+  serviceControllers.updateServiceById,
+)
 
-
-export {router as serviceRouter}
+export { router as serviceRouter }
